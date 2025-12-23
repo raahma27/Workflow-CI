dabe217661088f9f768749d102c3f20941bd70f3
raahma27 <niayaya027@gmail.com>
2025-12-23 17:17:02 +0700

    Fix dataset path for CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,43 +1,42 @@
-import pandas as pd
-import mlflow
-import mlflow.sklearn
+import os
+import pandas as pd 
+import mlflow 
+import mlflow.sklearn 
+from sklearn.model_selection import train_test_split 
+from sklearn.preprocessing import StandardScaler 
+from sklearn.linear_model import LogisticRegression 
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix 
 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+def main(): 
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
-def main():
-    # Load dataset
-    df = pd.read_csv("airline_preprocessed.csv")
-
-    # Split fitur & target
-    X = df.drop(columns=["satisfaction"])
-    y = df["satisfaction"]
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42, stratify=y
+    data_path = os.path.join(
+        BASE_DIR,
+        "airline_preprocessed",
+        "airline_preprocessed.csv"
     )
 
-    model = LogisticRegression(max_iter=1000)
-
-    with mlflow.start_run():
-        model.fit(X_train, y_train)
-
-        y_pred = model.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
-
-        mlflow.log_param("model", "LogisticRegression")
-        mlflow.log_param("max_iter", 1000)
-        mlflow.log_metric("accuracy", acc)
-        mlflow.sklearn.log_model(model, "model")
-
-        print("Accuracy:", acc)
-        print("\nClassification Report:")
-        print(classification_report(y_test, y_pred))
-        print("\nConfusion Matrix:")
-        print(confusion_matrix(y_test, y_pred))
-
-
-if __name__ == "__main__":
+    df = pd.read_csv(data_path) 
+    # Split fitur & target 
+    X = df.drop(columns=["satisfaction"]) 
+    y = df["satisfaction"] 
+    
+    X_train, X_test, y_train, y_test = train_test_split( 
+        X, y, test_size=0.2, random_state=42, stratify=y 
+        ) 
+    
+    scaler = StandardScaler() 
+    X_train = scaler.fit_transform(X_train) 
+    X_test = scaler.transform(X_test) 
+    
+    model = LogisticRegression(max_iter=1000) 
+    
+    mlflow.sklearn.autolog() 
+    
+    with mlflow.start_run(): 
+        model.fit(X_train, y_train) 
+        y_pred = model.predict(X_test) 
+        acc = accuracy_score(y_test, y_pred) 
+        
+if __name__ == "__main__": 
     main()
